e0e43b5f8092dbd5923a0825c8f552de25786d93
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-05 00:29:40 +0200

    Cleaned up the code

--- test/groovy/creditcard/CreditCardTester.groovy
+++ test/groovy/creditcard/CreditCardTester.groovy
@@ -5,28 +5,31 @@ import introtocs.main.groovy.creditcard.CreditCard
 def cards = new ArrayList<CreditCard>()
 
 CreditCard card1 = new CreditCard("Kóter Péter", 2500)
-CreditCard card2 = new CreditCard("Szabados Istvan", 1400)
-CreditCard card3 = new CreditCard("Horvath Alpar", 750)
+CreditCard card2 = new CreditCard("Szabados István", 1400)
+CreditCard card3 = new CreditCard("Horvath Alpár", 750)
 
-card2.makePurchase(200);
-card2.makePurchase(400);
-card2.makePurchase(150);
+card1.makePurchase(1000)
+card1.makePurchase(1500)
 
-card3.makePurchase(900);
+card2.makePurchase(200)
+card2.makePurchase(400)
+card2.makePurchase(150)
 
-card1.makePayment(50);
-card2.makePayment(25);
-card3.makePayment(75);
+card3.makePurchase(900)
 
-cards.add(card1);
-cards.add(card2);
-cards.add(card3);
+card1.makePayment(50)
+card2.makePayment(25)
+card3.makePayment(75)
 
-for (CreditCard card : cards) {
-    println("The cardholder is: " + card.getCardholder());
-    println("Total credit limit: " + card.getCreditLimit());
-    println("Total balance: " + card.getBalance());
-    println("Total reward points: " + card.calculateRewardPoints());
-    println("------------------------------");
+cards.add(card1)
+cards.add(card2)
+cards.add(card3)
+
+for (card in cards) {
+    println "The cardholder is: " + card.getCardholder()
+    println "Total credit limit: " + card.getCreditLimit()
+    println "Total balance: " + card.getBalance()
+    println "Total reward points: " + card.calculateRewardPoints()
+    println "------------------------------"
 }
 
