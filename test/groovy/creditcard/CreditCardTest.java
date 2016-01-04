e13d82e40499f38c30c9b310a7f3163213ad42a7
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-05 00:27:47 +0200

    Added more tests

--- test/groovy/creditcard/CreditCardTest.java
+++ test/groovy/creditcard/CreditCardTest.java
@@ -35,16 +35,23 @@ public class CreditCardTest {
     public void testMakePurchaseNoFee() {
         card1.makePurchase(450);
         assertEquals(450, card1.getBalance());
+        card2.makePurchase(600);
+        assertEquals(600, card2.getBalance());
     }
 
     @Test
     public void testMakePurchaseWithFee() {
         card1.makePurchase(700);
         assertEquals(750, card1.getBalance());
+        card2.makePurchase(1001);
+        assertEquals(1051, card2.getBalance());
     }
 
     @Test
     public void testMakePayment() {
+        card1.makePurchase(499);
+        card1.makePayment(500);
+        assertEquals(-1, card1.getBalance());
         card2.makePurchase(900);
         card2.makePayment(1000);
         assertEquals(-100, card2.getBalance());
@@ -52,6 +59,10 @@ public class CreditCardTest {
 
     @Test
     public void testCalculateRewardPoints() {
+        card1.makePurchase(1);
+        card1.makePurchase(1);
+        card1.makePurchase(1);
+        assertEquals(300, card1.calculateRewardPoints());
         card2.makePurchase(900);
         card2.makePurchase(10);
         card2.makePurchase(10);
@@ -60,3 +71,4 @@ public class CreditCardTest {
         assertEquals(500, card2.calculateRewardPoints());
     }
 }
+
